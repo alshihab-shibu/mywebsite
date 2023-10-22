b57974ce9cc0573a38fedb6f6add9532299518c1
alshihab7510 <alshihab7510@gmail.com>
2023-10-22 12:11:45 +0530

    under-maintaince-home

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,16 +16,17 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Alshihab</span>
+            Coming Soon
+            {/* Hi, I'm <span className='text-[#915EFF]'>Alshihab</span> */}
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-           Magento Developer
+           Under Construction
           </p>
         </div>
       </div>
 
-      <ComputersCanvas />
-
+      {/* <ComputersCanvas /> */}
+{/* 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
@@ -42,7 +43,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
