2edf4846d312544ecc56847ea8fe1e272fd9ff0d
alshihab7510 <alshihab7510@gmail.com>
2023-10-22 14:46:47 +0530

    About-Home updates

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,8 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import magento from "./magento2.png";
+
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -39,6 +41,7 @@ export {
   github,
   menu,
   close,
+  magento,
   css,
   docker,
   figma,
